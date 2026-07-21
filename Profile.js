27a50cf753deb4714d5c0e7d15ce22a2e35e4862
jecheyella <jessica.cheyzia@gmail.com>
2026-07-21 11:54:12 +0700

    again

--- Profile.js
+++ Profile.js
@@ -1,14 +1,9 @@
-import { auth, db } from "./firebase-config.js";
+import { auth } from "./firebase-config.js";
 
 import {
     onAuthStateChanged,
     signOut
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-import {
-    ref,
-    get
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 import { initUI } from "./UI.js";
 
@@ -20,16 +15,15 @@ ELEMENTS
 
 const name = document.getElementById("userName");
 const email = document.getElementById("userEmail");
-const logout = document.getElementById("profileLogoutBtn");
 
-const roleElement = document.getElementById("userRole");
-const badgeElement = document.getElementById("userRoleBadge");
+const logout =
+document.getElementById("profileLogoutBtn");
 
 /* =========================
 LOAD USER
 ========================= */
 
-onAuthStateChanged(auth, async (user) => {
+onAuthStateChanged(auth, (user) => {
 
     if (!user) {
 
@@ -38,48 +32,11 @@ onAuthStateChanged(auth, async (user) => {
 
     }
 
-    try {
+    name.textContent =
+        user.displayName || "Administrator";
 
-        const snapshot = await get(
-            ref(db, "Users/" + user.uid)
-        );
-
-        if (snapshot.exists()) {
-
-            const data = snapshot.val();
-
-            name.textContent =
-                data.fullName || "Unknown User";
-
-            email.textContent =
-                data.email || user.email;
-
-            // Show Administrator instead of Admin
-            let roleText = data.role || "Volunteer";
-
-            if (roleText === "Admin") {
-                roleText = "Administrator";
-            }
-
-            if (roleElement) {
-                roleElement.textContent = roleText;
-            }
-
-            if (badgeElement) {
-                badgeElement.textContent = roleText;
-            }
-
-        } else {
-
-            console.log("User data not found.");
-
-        }
-
-    } catch (error) {
-
-        console.error("Profile Error:", error);
-
-    }
+    email.textContent =
+        user.email;
 
 });
 
